875dc2996368b77801e5a5bf6f49ee1a65a2afae
Marco Boelling <53232140+4ty711@users.noreply.github.com>
2020-07-29 18:32:31 +0200

    changed landing page

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    currentPreview: 1,
-    currentLang: 'Clean + Simple',
-    langs: ['Clean + Simple', 'Extendable', 'Custom Syntax', 'CLI', 'Embeddable'],
-    previewItems: [{name: 'Objects', index: 1}, {name: 'Behaviours', index: 2}, {name: 'CRUD API', index: 3}, {name: 'Extendable', index: 4}]
-  }
+    el: '#app',
+    methods: {
+        clipboard: function(text) {
+        	text = this.installInstruction;
+            /* Get the text field */
+            var el = document.createElement("input");
+            el.style.display = 'none';
+            document.body.appendChild(el)
+            el.value = text;
+
+            /* Select the text field */
+            el.select();
+            el.setSelectionRange(0, 99999); /*For mobile devices*/
+
+            /* Copy the text inside the text field */
+            document.execCommand("copy");
+
+            this.copied = true;
+
+            setTimeout(() => {
+            	this.copied = false
+            }, 2000)
+        },
+        showInstall: function(text){
+        	this.installInstruction = text;
+        }
+    },
+    data: {
+    	copied:false,
+    	installInstruction:null,
+    	showInstallOptions:false,
+        currentPreview: 1,
+        currentLang: 'Clean + Simple',
+        langs: ['Clean + Simple', 'Extendable', 'Custom Syntax', 'CLI', 'Embeddable'],
+        previewItems: [{ name: 'Objects', index: 1 }, { name: 'Behaviours', index: 2 }, { name: 'CRUD API', index: 3 }, { name: 'Extendable', index: 4 }]
+    }
 })
