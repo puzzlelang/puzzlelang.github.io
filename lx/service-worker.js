4286b5456f49aeb4f01658cce5263f2f40e85a20
Marco Booelling <marco.boelling@web.de>
2020-08-03 15:14:06 +0200

    h

--- lx/service-worker.js
+++
@@ -1,8 +0,0 @@
-
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
-self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
- 
-});
